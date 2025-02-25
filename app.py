992bd58454c5345f1c0d4b7d3ba0bfe3e82ae05a
MickyBou <mickybou@gmail.com>
2025-02-25 16:31:38 +0100

    Update app.py

--- app.py
+++ app.py
@@ -79,10 +79,8 @@ def main():
     with st.sidebar:
         st.title("Menú:")
 
-    pdf_docs = st.file_uploader(
-        f"Choose up to {MAX_LINES} PDF files to upload", type=["pdf"], 
-        accept_multiple_files=True,
-    )
+    pdf_docs = st.file_uploader("Sube los archivos PDF y pulsa en el botón Enviar y Procesar", accept_multiple_files=True)
+
     st.html(
         """
         <style>
@@ -109,8 +107,6 @@ def main():
         """
     )
         
-        # pdf_docs = st.file_uploader("Sube los archivos PDF y pulsa en el botón Enviar y Procesar", accept_multiple_files=True)
-
         if st.button("Enviar y Procesar"):
             with st.spinner("Procesando..."):
                 raw_text = get_pdf_text(pdf_docs)
